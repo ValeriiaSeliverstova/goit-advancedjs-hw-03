5fa28863afd0f83b6737c94fc6a55dad444cbf53
ValeriiaSeliverstova <valeriia.seliverstova@what-software.com>
2025-03-22 22:50:52 +0000

    Deploying to gh-pages from @ ValeriiaSeliverstova/goit-advancedjs-hw-03@decaacb54635edd6722b3f666ed43b737c539012 🚀

---
+++ index.js
@@ -0,0 +1,28 @@
+import{i as s,S as d}from"./assets/vendor-5ObWk2rO.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function i(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(t){if(t.ep)return;t.ep=!0;const r=i(t);fetch(t.href,r)}})();const f="https://pixabay.com/api/",m="49479304-89a48f16eaa5319308b7de96a";function h(){document.querySelector(".loader").style.display="block"}function p(){document.querySelector(".loader").style.display="none"}function y(o){return h(),fetch(`${f}?key=${m}&q=${encodeURIComponent(o)}&image_type=photo&orientation=horizontal&safesearch=true`).then(e=>{if(!e.ok)throw new Error(`Failed to fetch! Status: ${e.status}`);return e.json()}).then(e=>e.hits).catch(e=>{throw e}).finally(()=>{p()})}function g(o){if(o.length===0)return s.error({title:"",message:"Sorry, there are no images matching your search query. Please try again!"});console.log(o);const e=document.querySelector(".gallery");e.innerHTML="";const i=o.map(({webformatURL:a,largeImageURL:t,tags:r,likes:n,views:c,comments:l,downloads:u})=>`<li class="gallery-item">
+	<a class="gallery-link" href="${t}">
+		<img
+			class="gallery-image"
+			src="${a}"
+			alt="${r}"
+			/>
+	</a>
+    <div class="info">
+        <div class="info-item">
+            <h3>Likes</h3>
+            <p>${n}</p>
+        </div>
+        <div class="info-item">
+            <h3>Views</h3>
+            <p>${c}</p>
+        </div>
+        <div class="info-item">
+            <h3>Comments</h3>
+            <p>${l}</p>
+        </div>
+        <div class="info-item">
+            <h3>Downloads</h3>
+            <p>${u}</p>
+        </div> 
+    </div>
+</li>`).join("");e.insertAdjacentHTML("beforeend",i),new d(".gallery-link",{captionsData:"alt",captionDelay:250,overlay:!0,overlayOpacity:.8})}const v=document.querySelector(".search-form"),L=document.querySelector('input[name="search-input"]');v.addEventListener("submit",o=>{o.preventDefault();const e=L.value.trim();if(!e)return s.error({title:"",message:"Please enter a search query!"});y(e).then(i=>{g(i)}).catch(i=>{throw i.message})});
+//# sourceMappingURL=index.js.map
